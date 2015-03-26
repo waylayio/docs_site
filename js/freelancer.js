2231941da889b51b45633e33df59941ccccaca70
pizurican <pizurica@gmail.com>
2015-03-26 19:39:53 +0100

    Grouped API pages, css/html changes to API pages

--- js/freelancer.js
+++ js/freelancer.js
@@ -35,3 +35,22 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+//BACK TO TOP
+
+    $(document).ready(function(){ 
+ 
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 100) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
+        }); 
+ 
+        $('.scrollup').click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 600);
+            return false;
+        });
+ 
+    });
